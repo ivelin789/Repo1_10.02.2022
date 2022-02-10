4c21ad542a330dd5231e416c56e29726f70059df
ivelin789 <1269112@nvna.eu>
2022-02-10 12:43:20 +0200

    COMMIT 1 10/02/2022

---
+++ typescript1.ts
@@ -0,0 +1,22 @@
+var masiv1 = [1,2,3,4,5,6,7,8,9,10]
+var masiv2 = [2,3,4,5,6,7,8,9,10,11]
+
+
+function add(a, b){
+    return a + b;
+}
+
+function add(a, b){
+    return a - b;
+}
+function add(a, b){
+    return a * b;
+}
+function add(a, b){
+    return a / b;
+}
+
+
+for(var i = 0; i < masiv1.length; i++) {
+    console.log("sum is" + add(masiv1[i], masiv2[i]))
+}
