80195eaee12a75af9bed14800b511bde72a1b2a0
ivelin789 <1269112@nvna.eu>
2022-02-24 02:13:57 +0200

    COMMIT 1 10/02/2022

--- typescript1.js
+++ typescript1.js
@@ -1,8 +1,29 @@
-var masiv1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var masiv2 = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-function add(a, b) {
+/*var masiv1 = [1,2,3,4,5,6,7,8,9,10]
+var masiv2 = [2,3,4,5,6,7,8,9,10,11]
+
+
+function add(a, b){
     return a + b;
 }
-for (var i = 0; i < masiv1.length; i++) {
-    console.log("sum is" + add(masiv1[i], masiv2[i]));
+
+function add(a, b){
+    return a - b;
 }
+function add(a, b){
+    return a * b;
+}
+function add(a, b){
+    return a / b;
+}
+
+
+for(var i = 0; i < masiv1.length; i++) {
+    console.log("sum is" + add(masiv1[i], masiv2[i]))
+}
+ */
+var Material;
+(function (Material) {
+    Material[Material["Glass"] = 0] = "Glass";
+    Material[Material["Plastic"] = 1] = "Plastic";
+    Material[Material["Ceramic"] = 2] = "Ceramic";
+})(Material || (Material = {}));
--- typescript1.ts
+++ typescript1.ts
@@ -1,4 +1,4 @@
-var masiv1 = [1,2,3,4,5,6,7,8,9,10]
+/*var masiv1 = [1,2,3,4,5,6,7,8,9,10]
 var masiv2 = [2,3,4,5,6,7,8,9,10,11]
 
 
@@ -20,3 +20,40 @@ function add(a, b){
 for(var i = 0; i < masiv1.length; i++) {
     console.log("sum is" + add(masiv1[i], masiv2[i]))
 }
+ */
+
+enum MATERIALS {
+    Glass = "Gla",
+    Plastic = "Pla",
+    Ceramic = "Cer"
+}
+
+interface Breakable {
+    material: MATERIALS;
+    break: () => {}
+}
+
+class GlassMug implements Breakable {
+    material: MATERIALS;
+    break: () => {}
+    constructor() {
+    type.mat = "Glass"
+    }
+}
+
+class CeramicMug implements Breakable {
+    material: MATERIALS;
+    break: () => {}
+    constructor() {
+    type.mat = "Ceramic"
+    }
+}
+
+class PlasticMug implements Breakable {
+    material: MATERIALS;
+    break: () => {}
+    constructor () {
+    type.mat = "Plastic"
+    }
+}
+
